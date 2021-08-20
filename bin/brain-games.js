ffcf63cc9bf425ffbe8bafebbd7540999f711b57
DenisFrolkin <denisfrolkin@gmail.com>
2021-08-20 22:06:05 +0300

    refactoring

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,5 +1,4 @@
 #!/usr/bin/env node
-import getUserName from '../src/cli.js';
+import helloGetUserName from '../src/cli.js';
 
-console.log('Welcome to the Brain Games!');
-console.log(`Hello, ${getUserName()}!`);
+HelloGetUserName();
