abb8a5fad7846a71163801b6aa69108f321b8aeb
DenisFrolkin <denisfrolkin@gmail.com>
2021-08-23 19:39:09 +0300

    add 'it finally works' files index and calc-game without linter-check

--- src/games/calc-game.js
+++ src/games/calc-game.js
@@ -4,15 +4,14 @@ import readlineSync from 'readline-sync';
 
 const currentGameIntro = 'What is the result of the expression?';
 
+const gameBody = () => {
 const arrOfChar = ['+', '-', '*'];
 const char = arrOfChar[randomElement(0, 3)];
 const num1 = randomElement(0, 20);
 const num2 = randomElement(0, 20);
-
 const currentGameQuestion = () => {
-   return (`${num1} ${char} ${num2}`);
+   return `${num1} ${char} ${num2}`;
 };
-
 const currentGameCorrectAnswer = () => {
    if (char === '+') {
       return (num1 + num2);
@@ -20,11 +19,12 @@ const currentGameCorrectAnswer = () => {
       return (num1 - num2);
    } else return (num1 * num2);
 };
+return [currentGameQuestion(), currentGameCorrectAnswer()];
+};
 
 const isResult = () => {
 
-gameEngine(currentGameIntro, currentGameQuestion, currentGameCorrectAnswer);
-
+gameEngine(currentGameIntro, gameBody);
 };
 
 export default isResult;
--- src/index.js
+++ src/index.js
@@ -1,19 +1,20 @@
 import randomElement from "./randomElement.js";
 import readlineSync from "readline-sync";
 
-const gameEngine = (currentGameIntro, currentGameQuestion, currentGameCorrectAnswer) => {
+const gameEngine = (currentGameIntro, gameBody) => {
     console.log('Welcome to the Brain-Games!');
     const name = readlineSync.question('May I have your name? ');
     console.log(`Hello ${name}!`);
     console.log(currentGameIntro);
     for (let i = 1; i <= 3; i += 1) {
-      const question = currentGameQuestion();
-      console.log(`Question: ${question}`);
-      const answer = readlineSync.question('Your answer: ');
-      const correctAnswer = currentGameCorrectAnswer();
-      if (correctAnswer == answer) {
+        const [currentGameQuestion, currentGameCorrectAnswer] = gameBody();
+        const question = currentGameQuestion;
+        console.log(`Question: ${question}`);
+        const answer = readlineSync.question('Your answer: ');
+        const correctAnswer = currentGameCorrectAnswer;
+        if (correctAnswer == answer) {
         console.log('Correct!');
-      } else return console.log(`'${answer}' is wrong answer ;(. correct answer was '${correctAnswer}'.\nLet's try again, ${name}!`);
+        } else return console.log(`'${answer}' is wrong answer ;(. correct answer was '${correctAnswer}'.\nLet's try again, ${name}!`);
     } return console.log(`Congratulations, ${name}!`);
 };
 export default gameEngine;
