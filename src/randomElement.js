4b957631311c79911cc2efcc5618e5fd90fbe7dc
Denis <denisfrolkin@gmail.com>
2021-09-21 02:08:29 +0300

    fix randomElement.js

--- src/randomElement.js
+++ src/randomElement.js
@@ -1,8 +1,7 @@
 const randomElement = (min, max) => {
   const floorMax = Math.floor(max);
   const ceilMin = Math.ceil(min);
-
-  return Math.floor(Math.random() * (floorMax - ceilMin) + ceilMin);
+  return Math.floor(Math.random() * (floorMax - ceilMin + 1) + ceilMin);
 };
 
 export default randomElement;
