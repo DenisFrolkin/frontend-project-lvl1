caea96a47cd61fa377a156404add6e8a82704268
DenisFrolkin <88797093+DenisFrolkin@users.noreply.github.com>
2021-09-22 09:38:38 +0300

    Update even-game.js

--- src/games/even-game.js
+++ src/games/even-game.js
@@ -3,12 +3,12 @@ import randomElement from '../randomElement.js';
 
 const gameRule = 'Answer "yes" if the number is even, otherwise answer "no".';
 
-const calculate = (number) => number % 2 === 0;
+const isEven = (number) => number % 2 === 0;
 
 const getGameData = () => {
   const question = randomElement(1, 1000000);
 
-  const correctAnswer = calculate(question) ? 'yes' : 'no';
+  const correctAnswer = isEven(question) ? 'yes' : 'no';
 
   return [question, correctAnswer];
 };
