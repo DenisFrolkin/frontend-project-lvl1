215d47a2e1a39be6eb3be568e3ab4e07f44aa7fb
DenisFrolkin <88797093+DenisFrolkin@users.noreply.github.com>
2021-09-24 16:08:12 +0300

    Update progression-game.js

--- src/games/progression-game.js
+++ src/games/progression-game.js
@@ -15,7 +15,7 @@ const getGameData = () => {
   const startNum = randomElement(-100, 101);
   const step = randomElement(-5, 6);
   const length = randomElement(5, 11);
-  const gap = randomElement(1, length - 1);
+  const hiddenIndex = randomElement(1, length - 1);
   const progression = makeProgression(startNum, step, length);
   const correctAnswer = String(progression[hiddenIndex]);
   progression[hiddenIndex] = '..';
