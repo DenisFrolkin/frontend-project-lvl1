aa037da21d0818878e833894a3414407a7b45193
DenisFrolkin <88797093+DenisFrolkin@users.noreply.github.com>
2021-09-24 16:05:08 +0300

    Update gcd-game.js

--- src/games/gcd-game.js
+++ src/games/gcd-game.js
@@ -3,9 +3,9 @@ import randomElement from '../randomElement.js';
 
 const gameRule = 'Find the greatest common divisor of given numbers.';
 
-const calculate = (x, y) => {
+const findGreatestCommonDivisor = (x, y) => {
   if (y !== 0) {
-    return calculate(y, x % y);
+    return findGreatestCommonDivisor(y, x % y);
   }
   return x;
 };
@@ -14,7 +14,7 @@ const getGameData = () => {
   const num1 = randomElement(1, 101);
   const num2 = randomElement(1, 101);
   const question = `${num1} ${num2}`;
-  const correctAnswer = String(calculate(num1, num2));
+  const correctAnswer = String(findGreatestCommonDivisor(num1, num2));
   return [question, correctAnswer];
 };
 
