788987faaf38ae3ee44420a650d19ba5007ef4a8
DenisFrolkin <88797093+DenisFrolkin@users.noreply.github.com>
2022-09-27 03:58:37 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import readlineSync from 'readline-sync';
 
 const roundsCount = 3;
 const gameEngine = (intro, getGameData) => {
-  console.log('/Welcome to the Brain-Games!/');
+  console.log('Welcome to the Brain-Games!');
   const name = readlineSync.question('May I have your name? ');
   console.log(`Hello ${name}!`);
   console.log(intro);
